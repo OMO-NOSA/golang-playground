866ac6e988adda1000823ff0c0244bd871bf8c44
Nosa Omorodion <nosaomorodion@surfer-172-30-11-39-hotspot.internet-for-guests.com>
2022-05-10 16:00:54 +0200

    Refactored tests

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := hello("Nosa")
-	want := "Hello, Nosa"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func (t testing.TB, got, want string)  {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to fine humans", func (t *testing.T)  {
+		got := hello("Nosa")
+		want := "Hello, Nosa"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("saying 'hello world' when an empty string is supplied", func (t *testing.T)  {
+		got := hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
 }

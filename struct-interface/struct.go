770a3d9c8555c98f42283ad4bb248662be418d5c
Nosa Omorodion <nosa.omorodion@getir.com>
2022-07-19 23:37:31 +0200

    Added methods definitions and refined tests

--- struct-interface/struct.go
+++ struct-interface/struct.go
@@ -1,19 +1,26 @@
 package structinterface
 
+import "math"
+
 type Rectangle struct {
-	width float64
+	width  float64
 	height float64
 }
 
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+type Circle struct {
+	Radius float64
 }
 
-func Area(width float64, height float64) float64 {
-	return width * height
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.width + rectangle.height)
 }
 
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
 
-
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
 
 //struct is a named collection of fields where data can be stored.
--- struct-interface/struct_test.go
+++ struct-interface/struct_test.go
@@ -13,12 +13,27 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Area(rectangle)
-	want := 50.0
+	
 
-	if got != want {
-		t.Errorf("got %.2f, want %.2f", got, want)
-	}
+	t.Run("testing area of rectangle", func(t *testing.T) {
+		
+		rectangle := Rectangle{10.0, 5.0}
+		got := rectangle.Area()
+		want := 50.0
+	
+		if got != want {
+			t.Errorf("got %.2f, want %.2f", got, want)
+		}
+	})
+
+	t.Run("testing area of circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 
 }

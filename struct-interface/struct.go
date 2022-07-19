1a29b2cce7614bee1e8c0819d7c3a27ddfbaf922
Nosa Omorodion <nosa.omorodion@getir.com>
2022-07-19 23:09:01 +0200

    Added a struct type

--- struct-interface/struct.go
+++ struct-interface/struct.go
@@ -1,5 +1,10 @@
 package structinterface
 
+type Rectangle struct {
+	width float64
+	height float64
+}
+
 func Perimeter(width float64, height float64) float64 {
 	return 2 * (width + height)
 }
@@ -7,3 +12,8 @@ func Perimeter(width float64, height float64) float64 {
 func Area(width float64, height float64) float64 {
 	return width * height
 }
+
+
+
+
+//struct is a named collection of fields where data can be stored.

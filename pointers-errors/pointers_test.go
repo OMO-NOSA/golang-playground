243323df11d4fc84059f80dc6bce225283b69647
Nosa Omorodion <nosa.omorodion@getir.com>
2022-07-26 08:42:20 +0200

    Added insufficient funds tests and code to check

--- pointers-errors/pointers_test.go
+++ pointers-errors/pointers_test.go
@@ -6,6 +6,16 @@ import (
 
 func TestWallet(t *testing.T) {
 
+	assertError := func(t testing.TB, got error, want string) {
+		t.Helper()
+		if got == nil {
+			t.Fatal("wanted an error but didn't get one")
+		}
+
+		if got.Error() != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
 	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
 		t.Helper()
 		got := wallet.Balance()
@@ -26,5 +36,14 @@ func TestWallet(t *testing.T) {
 
 		wallet.Withdraw(Bitcoin(10))
 		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Insufficient funds withdraw", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err, "Cannot withdraw , insufficient funds")
+		assertBalance(t, wallet, startingBalance)
 	})
 }

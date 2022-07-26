8ad1804f9167d2512bfcc51408711991fcd2d22b
Nosa Omorodion <nosa.omorodion@getir.com>
2022-07-26 09:22:00 +0200

    fixed error

--- pointers-errors/pointers_test.go
+++ pointers-errors/pointers_test.go
@@ -16,7 +16,7 @@ func TestWallet(t *testing.T) {
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
 		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})

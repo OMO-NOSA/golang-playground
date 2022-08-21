0eb2d8633ab463d4a1d7e741ae14b8305f593697
Nosa Omorodion <nosa.omorodion@getir.com>
2022-08-21 22:55:35 +0200

    refactored code to throw error when an existing word is added

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,13 +1,13 @@
 package maps
 
-import "errors"
-
-var (
-	ErrNotFound   = errors.New("could not find the word you were looking for")
-	ErrWordExists = errors.New("cannot add word because it already exists")
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
 )
 
 type Dictionary map[string]string
+
+type DictionaryErr string
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -19,6 +19,20 @@ func (d Dictionary) Search(word string) (string, error) {
 }
 
 func (d Dictionary) Add(word string, definition string) error {
-	d[word] = definition
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
 	return nil
+}
+
+func (e DictionaryErr) Error() string {
+	return string(e)
 }

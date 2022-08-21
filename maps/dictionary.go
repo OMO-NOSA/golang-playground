a7199a803a2394e1baa6c2a18204172b11a63b60
Nosa Omorodion <nosa.omorodion@getir.com>
2022-08-21 23:02:25 +0200

    Added update func to pass tests

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,8 +1,9 @@
 package maps
 
 const (
-	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
-	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
 )
 
 type Dictionary map[string]string
@@ -35,4 +36,19 @@ func (d Dictionary) Add(word string, definition string) error {
 
 func (e DictionaryErr) Error() string {
 	return string(e)
+}
+
+func (d Dictionary) Update(word string, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
 }

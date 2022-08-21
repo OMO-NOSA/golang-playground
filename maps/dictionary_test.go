c8d444c7eb49f92438804bbb878ab407ab406b38
Nosa Omorodion <nosa.omorodion@getir.com>
2022-08-21 21:55:51 +0200

    refactored code for better testing

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -6,25 +6,20 @@ func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
 	t.Run("known word", func(t *testing.T) {
-		got, _ := dictionary.Search("test")
-		want := "this is just a test"
+		_, got := dictionary.Search("test")
+		
 
-		assertStrings(t, got, want)
+		assertError(t, got, ErrNotFound)
 	})
 
 	t.Run("unknown word", func(t *testing.T) {
-		_, err := dictionary.Search("unknown")
-		want := "could not find the word you were looking for"
+		_, got := dictionary.Search("unknown")
 
-		if err == nil {
-			t.Fatal("expected to get an error.")
-		}
-
-		assertStrings(t, err.Error(), want)
+		assertError(t, got, ErrNotFound)
 	})
 }
 
-func assertStrings(t testing.TB, got, want string) {
+func assertError(t testing.TB, got, want error) {
 	t.Helper()
 
 	if got != want {
